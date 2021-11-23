120c73c6721f3ec1437019766deeeafafcd8594f
Vice2 <90139249+Vice2@users.noreply.github.com>
2021-11-23 12:26:15 +0100

    Initial Commit

---
+++ Reseguide/ReseguideApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReseguideApp.swift
+//  Reseguide
+//
+//  Created by Bashar Al Bagdadi on 2021-11-23.
+//
+
+import SwiftUI
+
+@main
+struct ReseguideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
